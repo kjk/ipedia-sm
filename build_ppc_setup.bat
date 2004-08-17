dc88cd0b6cc5982788f6ed90bebf0209c0e205e7
kjk <kjk@e1586a42-28db-0310-82f4-9a69bde6b091>
2004-08-17 07:12:54 +0000

    fix ppc build script
    git-svn-id: https://repo.kowalczyk.info/svn/sm_ipedia@88 e1586a42-28db-0310-82f4-9a69bde6b091

--- build_ppc_setup.bat
+++ build_ppc_setup.bat
@@ -5,9 +5,8 @@ copy readme_ppc.txt dist_ppc
 copy eula_sm.txt dist_ppc
 copy ipedia_ppc.inf dist_ppc
 copy ipedia_ppc.ini dist_ppc
-cd dist
-"C:\Program Files\Windows CE Tools\wce300\Smartphone 2002\tools\CabwizSP.exe" ipedia_ppc.inf
+cd dist_ppc
+"C:\Program Files\Windows CE Tools\wce300\Pocket PC 2002\support\ActiveSync\windows ce application installation\cabwiz\cabwiz.exe" ipedia_ppc.inf
 move ipedia_ppc.CAB ipedia_ppc_1_0.cab
-@rem ezsetup_sm -l english -i inoah_sm.ini -r readme.txt -e eula.txt -o iNoah_Smartphone_setup.exe
-ezsetup_sm -l english -i ipedia_ppc.ini -r readme_ppc.txt -e eula_sm.txt -o ipedia_ppc_1_0_setup.exe
-del *.txt *.ini *.inf ipedia.exe
+ezsetup -l english -i ipedia_ppc.ini -r readme_ppc.txt -e eula_sm.txt -o ipedia_ppc_1_0_setup.exe
+del *.txt *.ini *.inf *.DAT ipedia.exe
